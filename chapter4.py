48d1459929a3f8a6f5f57e870e1716b0d7698dbc
Laura Gamez <lgamez@cbpi.com.mx>
2022-10-29 16:14:15 -0500

    chapter 4

--- chapter4.py
+++ chapter4.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_wtf import FlaskForm
+from flask_bootstrap import Bootstrap
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
 class NameForm (FlaskForm):
@@ -13,13 +14,15 @@ class NameForm (FlaskForm):
 
 @app.route('/', methods =['GET', 'POST'])
 def index():
-     name = None
-     form = NameForm()
-     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-        return render_template('index_1.html', name=name)
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data: 
+            flash('Looks like you have changed your name!')
+            session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('user_temp_1.html', form=form, name=session.get('name'))
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    app.run(debug=True, port=8080)
  

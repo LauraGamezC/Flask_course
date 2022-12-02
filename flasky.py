859a04a78e4e973ca704ec587a88bd3110f240ed
Laura Gamez <lgamez@cbpi.com.mx>
2022-12-02 12:51:58 -0600

    Changes chapter 8

--- flasky.py
+++ flasky.py
@@ -1,15 +1,17 @@
 import os
 import click
-from app import create_app, db 
-from app.models import User, Role 
 from flask_migrate import Migrate
+from app import create_app, db
+from app.models import User, Role
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default') 
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
+
 
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Role=Role)
+
 
 @app.cli.command()
 @click.argument('test_names', nargs=-1)
